456212683e7fa01cacc5975e1c2bb779202767d8
K G ANIRUDDHA <eng21cs0218@dsu.edu.in>
2025-06-16 22:37:40 +0530

    Final commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="US_VISA",
+    version="0.0.0",
+    author="Aniruddha",
+    author_email="karnamaniruddha5@gmail.com",
+    packages=find_packages()
+)
